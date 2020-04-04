188a4a1f067d1888b26b2278e26f1155cd1198a5
Alicja Ciuńczyk <a.ciunczyk@gmail.com>
2020-04-04 11:21:05 +0200

    Add descriptions

--- calendar.py
+++ calendar.py
@@ -7,6 +7,8 @@ calendar = {}
 
 
 def welcome():
+    'Specify a user and print a welcome message.'
+
     print("Welcome " + OWNER_NAME + ".")
     print("The calendar is opening...")
     sleep(1)
@@ -17,18 +19,23 @@ def welcome():
 
 
 def start_calendar():
+    'Create a simple calendar with user input'
 
     welcome()
     start = True
     while start:
         user_choice = input("Type A to Add, U to Update, V to View, D to Delete, X to Exit: ")
         user_choice = user_choice.upper()
+        #   Viewing the calendar
         if user_choice == "V":
+            #   Check for a non-empty calendar
             if len(calendar.keys()) < 1:
                 print("Your calendar is empty.")
             else:
                 print(calendar)
+        #   Updating the calendar
         elif user_choice == "U":
+            #   Check for a non-empty calendar
             if len(calendar.keys()) < 1:
                 print("Your calendar is empty.")
                 try_again = input("Try Again? Y for Yes, N for No: ")
@@ -37,15 +44,18 @@ def start_calendar():
                     continue
                 else:
                     start = False
+            #   If the calendar has events
             else:
                 date = input("What date? ")
                 update = input("Enter update: ")
                 calendar[date] = update
                 print("Update was successful.")
                 print(calendar)
+        #   Adding events to the calendar
         elif user_choice == "A":
             event = input("Enter event: ")
             date = input("Enter date (MM/DD/YYYY): ")
+            # Check for a valid date
             if(len(date) > 10 or int(date[6:]) < int(strftime("%Y"))):
                 print("The date provided is invalid. The event cannot take place in the past.")
                 try_again = input("Try Again? Y for Yes, N for No: ")
@@ -58,7 +68,9 @@ def start_calendar():
                 calendar[date] = event
                 print("Event added.")
                 print(calendar)
+        #   Deleting events from the calendar
         elif user_choice == "D":
+            #   Check for a non-empty calendar
             if len(calendar.keys()) < 1:
                 print("Calendar is empty.")
             else:
@@ -71,12 +83,11 @@ def start_calendar():
                         break
                     else:
                         print("Cannot delete the event - event does not exist.")
+        #   Exiting the calendar
         elif user_choice == "X":
             start = False
             print("The program is exiting. Goodbye.")
+        #   Check for a valid command
         else:
             print("Invalid command. Exiting the program.")
             start = False
-
-
-start_calendar()
